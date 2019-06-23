b1b3d650ce60e3e6f3d1a2912b288014f53828e4
ttownsley <thomasdtownsley@gmail.com>
2019-06-23 11:51:11 -0500

    environment setup

---
+++ cvmain.py
@@ -0,0 +1,5 @@
+import numpy as np
+import cv2
+
+# Load an color image in grayscale
+img = cv2.imread('mycelium_1.jpg',0)
--- main.jl
+++
@@ -1,3 +0,0 @@
-using FileIO, Images, ImageView
-img = load("mycelium_1.jpg")
-imshow(img)
